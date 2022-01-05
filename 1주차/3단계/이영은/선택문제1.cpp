6c633104c519a8480ad46cfeec24214f2463ed6e
Youngeun Lee <yuye2002@naver.com>
2022-01-05 15:01:08 +0900

    1주차 3단계 이영은

---
+++ 1주차/3단계/이영은/선택문제1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+    구구단
+    https://www.acmicpc.net/problem/2739
+*/
+
+int main() {
+    int N;
+    std::cin >> N;
+    for(int i=1;i<10;i++){
+        std::cout << N << " * "
+                  << i << " = "
+                  << N*i << std::endl;
+    }
+    return 0;
+}
