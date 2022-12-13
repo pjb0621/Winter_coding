a331bb281c30fa07ae8987e6f6cfc3f56e0dbc5e
pjb0621 <pjb0621@naver.com>
2022-12-13 15:05:56 +0000

    12.14

---
+++ class 2/10845(queue).cpp
@@ -0,0 +1,119 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+typedef struct Queue{
+    struct Node* front;
+    struct Node* rear;
+    int count;
+}Queue;
+
+void init(Queue *q);
+int size(Queue *q);
+int is_empty(Queue *q);
+
+void init(Queue *q){
+    q->front = NULL;
+    q->rear = NULL;
+    q->count = 0;
+}
+
+int size(Queue *q){
+    return q->count;
+}
+
+int is_empty(Queue *q){
+    if(q->count == 0) return 1;
+    else return 0;
+}
+
+int front(Queue *q){
+    if(is_empty(q) == 1) return -1;
+    return q->front->data;
+}
+
+int back(Queue *q){
+    if(is_empty(q) == 1) return -1;
+    return q->rear->data;
+}
+
+void push(Que   ue *q, int data){
+    
+    // 새로운 노드를 먼저 생성함.
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode -> next = NULL;
+    newNode -> data = data;
+
+    if(is_empty(q) == 1){ // 큐가 비어있을때
+        q->front = newNode;
+    }
+    else{ // 큐가 비어있지 않을때
+        q->rear->next = newNode;
+    }
+    q->rear = newNode;
+    q->count++;
+}
+
+int pop(Queue *q){
+
+    if(is_em    pty(q) == 1) return -1;
+
+    struct Node *temp = q->front;
+    int data = temp->data;
+    q->front = temp->next;
+    free(temp);
+    q->count--;
+    return data;
+}
+
+int main(){
+    Queue q;
+    int SIZE;
+    scanf("%d", &SIZE);
+
+    char order[10000][12];
+    int num[SIZE];
+    int k = 0;
+    for(int i = 0; i < SIZE; i++){
+        scanf("%s", order[i]);
+        if(strcmp(order[i], "push")==0){
+            scanf("%d", &num[k++]);
+        }
+    }
+
+    k = 0;
+    int tmp;
+    for(int i = 0; i < SIZE; i++){
+        if(strcmp(order[i], "push")==0){
+            push(&q, num[k++]);
+        }
+        else if(strcmp(order[i], "front")==0){
+            tmp = front(&q);
+            printf("%d\n", tmp);
+        }
+        else if(strcmp(order[i], "back") == 0){
+            tmp = back(&q);
+            printf("%d\n", tmp);
+        }
+        else if(strcmp(order[i], "size") == 0){
+            tmp = size(&q);
+            printf("%d\n", tmp);
+        }
+        else if(strcmp(order[i], "empty") == 0){
+            tmp = is_empty(&q);
+            printf("%d\n", tmp);
+        }
+        else if(strcmp(order[i], "pop")==0){
+            tmp = pop(&q);
+            printf("%d\n", tmp);
+        }
+    }
+
+
+}
---
+++ class 2/10868.cpp
@@ -0,0 +1,164 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+typedef struct deque{
+    struct Node *front;
+    struct Node *rear;
+    int size;
+}deque;
+
+
+void init(deque *d);
+int size(deque *d);
+int is_empty(deque *d);
+
+
+void init(deque *d){
+    d->front = NULL;
+    d->rear = NULL;
+    d->size = 0;
+}
+
+int size(deque *d){
+    return d->size;
+}
+
+int is_empty(deque *d){
+    if(d->size == 0) return 1;
+    else return 0;
+}
+
+int front(deque *d){
+    if(is_empty(d) == 1) return -1;
+    return d->front->data;
+}
+
+int back(deque *d){
+    if(is_empty(d) == 1) return -1;
+    return d->rear->data;
+}
+
+void push_front(deque *d, int data){
+
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode -> next = NULL;
+    newNode -> prev = NULL;
+    newNode -> data = data;
+
+    if(is_empty(d) == 1){ // 덱이 비었을때
+        d-> rear = newNode;
+    }
+    else{
+        newNode -> next = d->front;
+        d->front -> prev = newNode;
+    }
+    d->front = newNode;
+    d->size++;
+}
+
+void push_back(deque *d, int data){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode -> next = NULL;
+    newNode -> prev = NULL;
+    newNode -> data = data;
+
+    if(is_empty(d) == 1){ // 덱이 비었을때
+        d-> front = newNode;
+    }
+    else{
+        newNode -> prev = d->rear;
+        d->rear -> next = newNode; // 기존의 rear 다음을 새로운 노드로
+    }
+    d->rear = newNode;
+    d->size++;
+}
+
+int pop_front(deque *d){
+
+    if(is_empty(d) == 1) // 덱이 비어있으면 
+        return -1;
+    
+    struct Node* temp = d->front;
+    int data = temp->data;
+    d->front = d->front->next;
+    d->front->prev= NULL;
+    temp -> next = NULL;
+    free(temp);
+    d->size--;
+    return data;
+}
+
+int pop_back(deque *d){
+
+    if(is_empty(d) == 1) return -1;
+
+    struct Node *temp = d->rear;
+    int data = temp -> data;
+    d-> rear = temp-> prev;
+    d->rear -> next = NULL;
+
+    temp->prev =NULL;
+    free(temp);
+    d->size--;
+    return data;
+}
+
+int main(){
+
+    deque d;
+    int SIZE;
+    scanf("%d", &SIZE);
+
+    char order[10000][12];
+    int num[10000];
+    int k = 0 ;
+    for(int i = 0; i< SIZE; i++){
+        scanf("%s", order[i]);
+        /* 숫자만 따로 다른 배열에 저장함 */
+        if(strcmp(order[i], "push_front")==0){
+            scanf("%d", &num[k++]);
+        }
+        else if(strcmp(order[i], "push_back")==0){
+            scanf("%d", &num[k++]);
+        }
+    }
+
+    k = 0;
+    int tmp;
+     for(int i = 0; i < SIZE; i++){
+        if(strcmp(order[i], "push_front")==0){
+            push_front(&d, num[k++]);
+        }
+        else if(strcmp(order[i], "push_back")==0){
+            push_back(&d,num[k++]);
+        }
+        else if(strcmp(order[i], "back") == 0){
+            tmp = back(&d);
+            printf("%d\n", tmp);
+        }
+        else if(strcmp(order[i], "size") == 0){
+            tmp = size(&d);
+            printf("%d\n", tmp);
+        }
+        else if(strcmp(order[i], "empty") == 0){
+            tmp = is_empty(&d);
+            printf("%d\n", tmp);
+        }
+        else if(strcmp(order[i], "pop_front")==0){
+            tmp = pop_front(&d);
+            printf("%d\n", tmp);
+        }
+        else if(strcmp(order[i], "pop_back")==0){
+            tmp = pop_back(&d);
+            printf("%d\n", tmp);
+        }
+    }
+}
